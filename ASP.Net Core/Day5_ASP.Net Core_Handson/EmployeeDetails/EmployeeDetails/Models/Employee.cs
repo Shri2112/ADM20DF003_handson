8da85538995841ad48a435a789aeff3a3aa3bce5
Shri2112 <79863766+Shri2112@users.noreply.github.com>
2021-03-05 18:50:38 +0530

    .NetCore

---
+++ ASP.Net Core/Day5_ASP.Net Core_Handson/EmployeeDetails/EmployeeDetails/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeDetails.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public decimal salary { get; set; }
+        public bool permanent { get; set; }
+
+        public Employee(int id, string name, decimal salary, bool permanent)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+            this.permanent = permanent;
+
+        }
+    }
+}
---
+++ ASP.Net Core/Day5_ASP.Net Core_Handson/ListOfOrganizations/ListOfOrganizations/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using ListOfOrganizations.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListOfOrganizations.Controllers
+{
+    public class HomeController : Controller
+    {
+        public List<String> l1 = new List<string> { "Microsoft www.microsoft.com", "CTS www.cognizant.com", " Google", "Amazon", "TCS" };
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult ItOrganizations()
+        {
+
+            return View(l1);
+        }
+    }
+}
